06664fbca81e8892cfa9c01eaa16f90497cd1be3
MahletH <mahletdereje23@gmail.com>
2021-02-06 18:43:06 +0300

    update on resource model

--- resourcify/lib/models/resource_model.dart
+++ resourcify/lib/models/resource_model.dart
@@ -1,36 +1,56 @@
+import 'dart:ffi';
+
 import 'package:equatable/equatable.dart';
 
 class Resource extends Equatable{
-  String id;
-  String votes;
-  String resourceName;
-  String uploadedBy;
-  String uploadedDate;
-  String fileType;
-  String fileSize;
-  String category;
+  final String id;
+  final String resourceName;
+  final String uploadedBy;
+  final String year;
+  final String department;
+  final String subject;
+  final int likes;
+  final int dislikes;
+  final String fileType;
+  final Float fileSize;
+  final bool isLiked;
+  final bool isDisliked;
+  final List<String> files;
   Resource(
       {this.id,
-      this.votes,
       this.resourceName,
       this.uploadedBy,
-      this.uploadedDate,
       this.fileSize,
       this.fileType,
-      this.category});
+      this.year,
+      this.department,
+      this.subject,
+      this.likes,
+      this.dislikes,
+      this.isLiked,
+      this.isDisliked,
+      this.files
+      });
   factory Resource.fromJson(Map<String, dynamic> json) {
     return Resource(
         id: json['_id'] as String,
-        votes: json['votes'] as String,
         resourceName:json['resource_name'] as String,
         fileType:json['file_type'] as String,
-        fileSize:json['file_size'] as String,
-        category:json['category'] as String,
-        uploadedBy: json['user_id']['user_name'] as String,
-        uploadedDate: json['uploaded_date'] as String);
+        fileSize:json['file_size'] as Float,
+        likes: json['likes'] as int,
+        dislikes: json['dislikes'] as int,
+        year: json['year'],
+        subject: json['subject'],
+        department: json['department'],
+        uploadedBy: json['created_by'],
+        isLiked: json['is_liked'] as bool,
+        isDisliked: json['is_disliked'] as bool,
+        files: json['files'] as List<String>,
+
+        );
   }
   
   @override
   List<Object> get props =>
-      [id, votes, resourceName, fileType, fileSize,uploadedBy, uploadedDate, category];
+      [id, resourceName, fileType, fileSize,uploadedBy, likes, dislikes, year, department, subject];
 }
